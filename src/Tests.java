a81bc5a0e364cafd192a52f668c46385a9000f85
Jonathan Sundqvist <jonte.gubbe@hotmail.com>
2017-05-18 17:22:22 +0200

    Added constructor for test class and first test for user story 1

--- src/Tests.java
+++ src/Tests.java
@@ -3,5 +3,16 @@ import org.junit.Test;
 
 public class Tests
 {
-	
+	Bowling bowling;
+
+	public Tests()
+	{
+		bowling = new Bowling();
+	}
+
+	@Test
+	public void testFrame()
+	{
+		assertNotNull(bowling.getFrame(0));
+	}
 }
