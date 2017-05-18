fac09e90ba20014252f76f6aaa4e7402b27d714b
Jonathan Sundqvist <jonte.gubbe@hotmail.com>
2017-05-18 17:31:46 +0200

    Implementation of code for test case testFrame() for user story 1

--- src/Bowling.java
+++ src/Bowling.java
@@ -3,5 +3,33 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Bowling
 {
-	
+	private java.util.List<int[]> frames;
+
+	public Bowling()
+	{
+		frames = new ArrayList<int[]>();
+	}
+
+	// random points
+	public void initRandomPoints()
+	{
+		frames.clear();
+		for (int i = 0; i < 10; i++)
+		{
+			int first = ThreadLocalRandom.current().nextInt(0, 10 + 1);
+			int second = ThreadLocalRandom.current().nextInt(0, 10 - first + 1);
+			frames.add(new int[]{first, second});
+		}
+	}
+
+	public int[] getFrame(int frame)
+	{
+		if (frame > 0 && frame < frames.size())
+		{
+			return frames.get(frame);
+		}
+
+		return new int[]{0, 0};
+
+	}
 }
