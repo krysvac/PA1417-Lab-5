bd0da81241786f54f22877b09f233ceb1787a40c
Jonathan Sundqvist <jonte.gubbe@hotmail.com>
2017-05-18 17:26:55 +0200

    Test cases for user story 4 - 14. Tests various outcomes with multiple strikes, strike at the end etc.

--- src/Tests.java
+++ src/Tests.java
@@ -32,4 +32,67 @@ public class Tests
 		bowling.initRandomPoints();
 		assertEquals(bowling.getAmountOfFrames(), 10);
 	}
+
+	@Test
+	public void testGameScore1()
+	{
+		bowling.init81points();
+		assertEquals(bowling.score(), 81);
+	}
+
+	@Test
+	public void testGameScore2()
+	{
+		bowling.init88points();
+		assertEquals(bowling.score(), 88);
+	}
+
+	@Test
+	public void testGameScore3()
+	{
+		bowling.init94points();
+		assertEquals(bowling.score(), 94);
+	}
+
+	@Test
+	public void testGameScore4()
+	{
+		bowling.init103points();
+		assertEquals(bowling.score(), 103);
+	}
+
+	@Test
+	public void testGameScore5()
+	{
+		bowling.init279points();
+		assertEquals(bowling.score(), 279);
+	}
+
+	@Test
+	public void testGameScore6()
+	{
+		bowling.init300points();
+		assertEquals(bowling.score(), 300);
+	}
+
+	@Test
+	public void testGameScore7()
+	{
+		bowling.init135points();
+		assertEquals(bowling.score(), 135);
+	}
+
+	@Test
+	public void testGameScore8()
+	{
+		bowling.init98points();
+		assertEquals(bowling.score(), 98);
+	}
+
+	@Test
+	public void testGameScore9()
+	{
+		bowling.init93points();
+		assertEquals(bowling.score(), 93);
+	}
 }
