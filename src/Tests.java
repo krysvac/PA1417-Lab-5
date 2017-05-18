8636bfd1e55a4edf747f1e1db22dde922cc435c9
Jonathan Sundqvist <jonte.gubbe@hotmail.com>
2017-05-18 17:24:43 +0200

    added test for framescore. User story 2

--- src/Tests.java
+++ src/Tests.java
@@ -15,4 +15,14 @@ public class Tests
 	{
 		assertNotNull(bowling.getFrame(0));
 	}
+
+	@Test
+	public void testFrameScore()
+	{
+		bowling.initRandomPoints();
+		int firstframe[] = bowling.getFrame(0);
+		int value = bowling.getFrameScore(0);
+
+		assertEquals(firstframe[0] + firstframe[1], value);
+	}
 }
