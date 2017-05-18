b36bf0d3661331c4c8a14e7da93a6819bf02bb1a
Jonathan Sundqvist <jonte.gubbe@hotmail.com>
2017-05-18 17:41:14 +0200

    Implementation of code for testcases testGameScore1-9 for userstories 4-14

--- src/Bowling.java
+++ src/Bowling.java
@@ -22,6 +22,155 @@ public class Bowling
 		}
 	}
 
+	// 81 points
+	public void init81points()
+	{
+		frames.clear();
+		frames.add(new int[]{1, 5});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{7, 2});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{4, 4});
+		frames.add(new int[]{5, 3});
+		frames.add(new int[]{3, 3});
+		frames.add(new int[]{4, 5});
+		frames.add(new int[]{8, 1});
+		frames.add(new int[]{2, 6});
+	}
+
+	// 94 points
+	public void init94points()
+	{
+		frames.clear();
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{7, 2});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{4, 4});
+		frames.add(new int[]{5, 3});
+		frames.add(new int[]{3, 3});
+		frames.add(new int[]{4, 5});
+		frames.add(new int[]{8, 1});
+		frames.add(new int[]{2, 6});
+	}
+
+	// 88 points
+	public void init88points()
+	{
+		frames.clear();
+		frames.add(new int[]{1, 9});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{7, 2});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{4, 4});
+		frames.add(new int[]{5, 3});
+		frames.add(new int[]{3, 3});
+		frames.add(new int[]{4, 5});
+		frames.add(new int[]{8, 1});
+		frames.add(new int[]{2, 6});
+	}
+
+	// 103 points
+	public void init103points()
+	{
+		frames.clear();
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{4, 6});
+		frames.add(new int[]{7, 2});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{4, 4});
+		frames.add(new int[]{5, 3});
+		frames.add(new int[]{3, 3});
+		frames.add(new int[]{4, 5});
+		frames.add(new int[]{8, 1});
+		frames.add(new int[]{2, 6});
+	}
+
+	// 300 points
+	public void init300points()
+	{
+		frames.clear();
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+	}
+
+	// 279 points
+	public void init279points()
+	{
+		frames.clear();
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{9, 1});
+		frames.add(new int[]{10, 0});
+	}
+
+	// 135 points
+	public void init135points()
+	{
+		frames.clear();
+		frames.add(new int[]{6, 3});
+		frames.add(new int[]{7, 1});
+		frames.add(new int[]{8, 2});
+		frames.add(new int[]{7, 2});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{6, 2});
+		frames.add(new int[]{7, 3});
+		frames.add(new int[]{10, 0});
+		frames.add(new int[]{8, 0});
+		frames.add(new int[]{7, 3});
+		frames.add(new int[]{10, 0});
+	}
+
+	// 98 points
+	public void init98points()
+	{
+		frames.clear();
+		frames.add(new int[]{8, 2});
+		frames.add(new int[]{5, 5});
+		frames.add(new int[]{7, 2});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{4, 4});
+		frames.add(new int[]{5, 3});
+		frames.add(new int[]{3, 3});
+		frames.add(new int[]{4, 5});
+		frames.add(new int[]{8, 1});
+		frames.add(new int[]{2, 6});
+	}
+
+	// 93 points
+	public void init93points()
+	{
+		frames.clear();
+		frames.add(new int[]{1, 5});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{7, 2});
+		frames.add(new int[]{3, 6});
+		frames.add(new int[]{4, 4});
+		frames.add(new int[]{5, 3});
+		frames.add(new int[]{3, 3});
+		frames.add(new int[]{4, 5});
+		frames.add(new int[]{8, 1});
+		frames.add(new int[]{2, 8});
+		frames.add(new int[]{10, 0});
+	}
+
 	public int[] getFrame(int frame)
 	{
 		if (frame > 0 && frame < frames.size())
@@ -42,5 +191,75 @@ public class Bowling
 	public int getAmountOfFrames()
 	{
 		return frames.size();
+	}
+
+	public int score()
+	{
+		int sum = 0;
+
+		for (int i = 0; i < 10; i++)
+		{
+			sum += frames.get(i)[0];
+			sum += frames.get(i)[1];
+			if (isStrike(i))
+			{
+				sum += strikePoints(i);
+			}
+			else if (isSpare(i))
+			{
+				sum += sparePoints(i);
+			}
+		}
+
+		return sum;
+	}
+
+	private boolean isStrike(int frame)
+	{
+		return frames.get(frame)[0] == 10;
+	}
+
+	private boolean isSpare(int frame)
+	{
+		int arr[] = frames.get(frame);
+		return arr[0] + arr[1] == 10;
+	}
+
+	private int sparePoints(int frame)
+	{
+		if (frame > -1 & frame + 1 < frames.size())
+		{
+			return frames.get(frame + 1)[0];
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	private int strikePoints(int frame)
+	{
+		if (frame > -1 && frame < 10)
+		{
+			if (frame + 1 < frames.size())
+			{
+				if (isStrike(frame + 1))
+				{
+					if (frame + 2 < frames.size())
+					{
+						return frames.get(frame + 1)[0] + frames.get(frame + 2)[0];
+					}
+					else
+					{
+						return frames.get(frame + 1)[0];
+					}
+				}
+				else
+				{
+					return frames.get(frame + 1)[0] + frames.get(frame + 1)[1];
+				}
+			}
+		}
+		return 0;
 	}
 }
